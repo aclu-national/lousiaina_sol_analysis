065e3ae43ebcb8fd5daa4c5e2b6b3ac7382f1f35
appelson <124807267+appelson@users.noreply.github.com>
2023-09-29 16:53:40 -0400

    Update master_script.R

--- master_script.R
+++ master_script.R
@@ -1,4 +1,4 @@
 # Running scripts in order
-source(here::here("data_creation.R"))
-source(here::here("data_analysis.R"))
-source(here::here("data_statistics.R"))
+source(here::here("scripts/data_creation.R"))
+source(here::here("scripts/data_analysis.R"))
+source(here::here("scripts/data_statistics.R"))
